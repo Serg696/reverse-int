83cc31262584020710ed62dd5c02f90c1ac7ccd0
Serg696 <88836338+Serg696@users.noreply.github.com>
2022-01-17 16:29:56 +0300

    Edit index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+	let num = String(n).split('');
+    	let znak;
+    	if (num[0] === "-"){
+      		znak = num[0];
+      		num = num.slice(1);
+      		//console.log("Minus" + znak);
+    	}
+    	//num = znak ? znak + num.reverse().join('') : num.reverse().join('');
+	num = num.reverse().join('');
+	return Number(num)
 }
